04587d1c4a27ad2b31c26b118e7cbe77d203ff1a
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2022-12-18 10:04:54 +0800

    create User Model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+                type: String,
+                required: true,
+                unique: true,
+                match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: '',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ]
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+})
